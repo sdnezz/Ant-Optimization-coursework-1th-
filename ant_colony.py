aa9f1ea096411e759f9728d62aa46cfe3e969901
sdnezz <twinknezz@gmail.com>
2024-11-26 17:19:54 +0300

    Changed process'es iterations & vizualisation
    - Изменена логика алгоримта, теперь расчитывается строго по феромонам краткий путь, а не по его длине
    - На каждой итерации несколько муравьев случайным образом выбирают путь, не имея "привлекательных" в начале
    - Отображение наглядно найденного пути и исчезновение феромонов на ребрах, при испарении

--- ant_colony.py
+++ ant_colony.py
@@ -1,83 +1,116 @@
+import random
 import numpy as np
 
+
 class AntColonyAlgorithm:
-    def __init__(self, graph, evaporation_rate=0.5, pheromone_intensity=1.0, alpha=1, beta=2):
-        """
-        Инициализация муравьиного алгоритма.
-        :param graph: Граф в формате NetworkX.
-        :param evaporation_rate: Коэффициент испарения феромонов (0 < evaporation_rate <= 1).
-        :param pheromone_intensity: Интенсивность феромонов.
-        :param alpha: Вес влияния феромонов.
-        :param beta: Вес влияния расстояния.
-        """
+    def __init__(self, graph, evaporation_rate, pheromone_intensity, alpha, beta):
         self.graph = graph
         self.evaporation_rate = evaporation_rate
         self.pheromone_intensity = pheromone_intensity
         self.alpha = alpha
         self.beta = beta
 
-        # Инициализация феромонов на всех рёбрах
-        for u, v in self.graph.edges:
-            self.graph[u][v]['pheromone'] = 1.0  # Начальное значение феромонов
+    def calculate_path_length(self, path):
+        """Вычисляет длину пути."""
+        return sum(self.graph[u][v]['weight'] for u, v in zip(path, path[1:]))
 
-    def calculate_probabilities(self, current_node, visited_nodes):
-        """
-        Рассчитывает вероятности перехода в соседние узлы.
-        :param current_node: Текущий узел муравья.
-        :param visited_nodes: Узлы, которые муравей уже посетил.
-        :return: Словарь {узел: вероятность}.
-        """
-        probabilities = {}
-        neighbors = [n for n in self.graph.neighbors(current_node) if n not in visited_nodes]
+    def generate_ant_path(self, start_node, end_node):
+        """Генерация пути для одного муравья."""
+        current_node = start_node
+        path = [current_node]
+        visited_nodes = {current_node}
 
-        if not neighbors:  # Если нет доступных соседей
-            return probabilities
+        while current_node != end_node:
+            neighbors = list(self.graph.neighbors(current_node))
+            probabilities = self.calculate_probabilities(current_node, neighbors, visited_nodes)
 
-        total = 0
+            if not probabilities:  # Если нет доступных узлов, тупик
+                return path
+
+            next_node = random.choices(neighbors, weights=probabilities)[0]
+            path.append(next_node)
+            visited_nodes.add(next_node)
+            current_node = next_node
+
+        return path
+
+    def run_iteration(self, num_ants, start_node, end_node=None):
+        all_paths = []
+        best_path = None
+        best_path_length = float('inf')
+
+        for _ in range(num_ants):
+            path, path_length = self.find_path(start_node, end_node)
+            all_paths.append((path, path_length))
+            if path_length < best_path_length:
+                best_path = path
+                best_path_length = path_length
+
+        return all_paths, best_path
+
+    def find_path(self, start_node, end_node=None):
+        current_node = start_node
+        visited_nodes = {current_node}
+        path = [current_node]
+        total_length = 0
+
+        while True:
+            if end_node is not None and current_node == end_node:
+                break
+
+            next_node = self.select_next_node(current_node, visited_nodes)
+            if next_node is None:  # Dead end
+                return path, float('inf')
+
+            visited_nodes.add(next_node)
+            path.append(next_node)
+            total_length += self.graph[current_node][next_node]['weight']
+            current_node = next_node
+
+        return path, total_length
+
+    def select_next_node(self, current_node, visited_nodes):
+        neighbors = [node for node in self.graph.neighbors(current_node) if node not in visited_nodes]
+
+        if not neighbors:
+            return None
+
+        probabilities = []
         for neighbor in neighbors:
             pheromone = self.graph[current_node][neighbor]['pheromone']
             distance = self.graph[current_node][neighbor]['weight']
-            attractiveness = (pheromone ** self.alpha) * ((1 / distance) ** self.beta)
-            probabilities[neighbor] = attractiveness
-            total += attractiveness
+            if distance == 0:  # Защита от деления на ноль
+                probabilities.append(0)
+            else:
+                probabilities.append((pheromone ** self.alpha) * ((1 / distance) ** self.beta))
 
-        # Нормализация вероятностей
-        for neighbor in probabilities:
-            probabilities[neighbor] /= total
+        probabilities = np.array(probabilities)
+        probabilities /= probabilities.sum()
 
-        return probabilities
+        return random.choices(neighbors, weights=probabilities, k=1)[0]
 
-    def update_pheromones(self, paths, best_path):
-        """
-        Обновляет феромоны на рёбрах графа.
-        :param paths: Все пройденные пути (список списков узлов).
-        :param best_path: Кратчайший путь, найденный за итерацию.
-        """
-        # Испарение феромонов
+    def update_pheromones(self, all_paths, best_path=None):
+        """Обновляет феромоны на основе пройденных путей."""
         for u, v in self.graph.edges:
+            print(f"Before evaporation ({u}, {v}): {self.graph[u][v]['pheromone']}")  # Перед обновлением
             self.graph[u][v]['pheromone'] *= (1 - self.evaporation_rate)
 
-        # Увеличение феромонов на пройденных рёбрах
-        for path in paths:
-            path_length = self.calculate_path_length(path)
-            for i in range(len(path) - 1):
-                u, v = path[i], path[i + 1]
+        for path, path_length in all_paths:
+            for u, v in zip(path, path[1:]):
                 self.graph[u][v]['pheromone'] += self.pheromone_intensity / path_length
+                print(f"After update ({u}, {v}): {self.graph[u][v]['pheromone']}")  # После обновления
 
-        # Увеличение феромонов на лучшем пути
-        best_length = self.calculate_path_length(best_path)
-        for i in range(len(best_path) - 1):
-            u, v = best_path[i], best_path[i + 1]
-            self.graph[u][v]['pheromone'] += 2 * (self.pheromone_intensity / best_length)
+        if best_path:
+            best_length = sum(self.graph[u][v]['weight'] for u, v in zip(best_path, best_path[1:]))
+            for u, v in zip(best_path, best_path[1:]):
+                self.graph[u][v]['pheromone'] += 2 * (self.pheromone_intensity / best_length)
+                print(f"Boost best path ({u}, {v}): {self.graph[u][v]['pheromone']}")  # Усиление лучшего пути
 
-    def calculate_path_length(self, path):
-        """
-        Вычисляет длину пути.
-        :param path: Список узлов пути.
-        :return: Длина пути.
-        """
-        length = 0
-        for i in range(len(path) - 1):
-            u, v = path[i], path[i + 1]
-            length += self.graph[u][v]['weight']
-        return length
+    def evaporate_pheromones(self):
+        """Уменьшает феромон на всех рёбрах графа."""
+        for u, v in self.graph.edges:
+            self.graph[u][v]['pheromone'] *= (1 - self.evaporation_rate)
+            # Гарантируем, что феромон не опустится ниже минимального уровня
+            if self.graph[u][v]['pheromone'] < 0.1:
+                self.graph[u][v]['pheromone'] = 0.1
+
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import networkx as nx
 import pyqtgraph as pg
 from ant_colony import AntColonyAlgorithm
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QComboBox, QFileDialog, QLineEdit, QFormLayout, QGroupBox
+from PyQt5.QtCore import QTimer
+
 
 class GraphWindow(QWidget):
     def __init__(self):
@@ -19,8 +21,9 @@ class GraphWindow(QWidget):
 
         # Изначально граф пустой
         self.graph = nx.Graph()
-        self.ant_colony = None  # Экземпляр муравьиного алгоритма
+        self.ant_colony = None
         self.start_node = None
+        self.end_node = None
 
         # Инициализация элементов интерфейса
         self.create_controls()
@@ -29,7 +32,7 @@ class GraphWindow(QWidget):
         # Создаем виджет для графика с использованием pyqtgraph
         self.plot_widget = pg.PlotWidget()
         self.layout.addWidget(self.plot_widget)
-        self.plot_widget.setBackground('w')  # Устанавливаем белый фон для графика
+        self.plot_widget.setBackground('w')
 
     def load_styles(self, style_file):
         """Загружает стили из указанного файла."""
@@ -39,21 +42,28 @@ class GraphWindow(QWidget):
     def create_controls(self):
         controls_layout = QFormLayout()
 
-        # Выпадающий список для выбора начальной точки
+        # Начальная точка
         self.start_node_combo = QComboBox(self)
         self.start_node_combo.currentIndexChanged.connect(self.update_start_node)
         controls_layout.addRow("Начальная точка:", self.start_node_combo)
+
+        # Конечная точка
+        self.end_node_combo = QComboBox(self)
+        self.end_node_combo.currentIndexChanged.connect(self.update_end_node)
+        controls_layout.addRow("Конечная точка:", self.end_node_combo)
 
         # Параметры алгоритма
         self.evaporation_rate_input = QLineEdit("0.5", self)
         self.pheromone_intensity_input = QLineEdit("1.0", self)
         self.alpha_input = QLineEdit("1", self)
         self.beta_input = QLineEdit("2", self)
+        self.num_ants_input = QLineEdit("10", self)
 
         controls_layout.addRow("Коэффициент испарения:", self.evaporation_rate_input)
         controls_layout.addRow("Интенсивность феромонов:", self.pheromone_intensity_input)
         controls_layout.addRow("Вес феромонов (alpha):", self.alpha_input)
         controls_layout.addRow("Вес расстояния (beta):", self.beta_input)
+        controls_layout.addRow("Количество муравьёв:", self.num_ants_input)
 
         controls_group = QGroupBox("Настройки алгоритма")
         controls_group.setLayout(controls_layout)
@@ -71,43 +81,32 @@ class GraphWindow(QWidget):
         buttons_layout.addWidget(self.start_button)
 
         self.stop_button = QPushButton('Остановка')
+        self.stop_button.clicked.connect(self.stop_algorithm)
         buttons_layout.addWidget(self.stop_button)
-
-        self.reset_button = QPushButton('Сброс')
-        self.reset_button.clicked.connect(self.reset_graph)
-        buttons_layout.addWidget(self.reset_button)
-
-        self.update_params_button = QPushButton('Изменить параметры')
-        self.update_params_button.clicked.connect(self.update_parameters)
-        buttons_layout.addWidget(self.update_params_button)
 
         self.layout.addLayout(buttons_layout)
 
     def load_graph(self):
-        """Загрузка графа из файла."""
         file_name, _ = QFileDialog.getOpenFileName(self, 'Открыть файл графа', '', 'Text Files (*.txt);;All Files (*)')
-
         if file_name:
             self.graph = self.load_graph_from_file(file_name)
+            self.positions = nx.spring_layout(self.graph)
             self.initialize_pheromones()
             self.update_start_node_combo()
+            self.update_end_node_combo()
             self.start_node = self.get_start_node()
-            self.update_canvas()
+            self.end_node = self.get_end_node()
+            self.visualize_initial_graph()
 
     def load_graph_from_file(self, file_path):
-        """Загрузка графа из файла."""
         G = nx.Graph()
-        try:
-            with open(file_path, 'r') as file:
-                for line in file:
-                    u, v, weight = map(int, line.split())
-                    G.add_edge(u, v, weight=weight, pheromone=1.0)
-        except Exception as e:
-            print(f"Ошибка при чтении файла: {e}")
+        with open(file_path, 'r') as file:
+            for line in file:
+                u, v, weight = map(int, line.split())
+                G.add_edge(u, v, weight=weight, pheromone=1.0)
         return G
 
     def initialize_pheromones(self):
-        """Инициализация феромонов на всех рёбрах графа."""
         for u, v in self.graph.edges:
             self.graph[u][v]['pheromone'] = 1.0
 
@@ -116,75 +115,220 @@ class GraphWindow(QWidget):
         for node in sorted(self.graph.nodes):
             self.start_node_combo.addItem(str(node))
 
-    def get_start_node(self):
-        """Получает текущую начальную точку из выпадающего списка."""
-        current_text = self.start_node_combo.currentText()
-        if current_text:
-            return int(current_text)
-        return None
-
-    def update_start_node(self):
-        self.start_node = self.get_start_node()
-        if self.start_node is not None:
-            self.update_canvas()
-
-    def update_parameters(self):
-        """Обновляет параметры алгоритма."""
-        self.evaporation_rate = float(self.evaporation_rate_input.text())
-        self.pheromone_intensity = float(self.pheromone_intensity_input.text())
-        self.alpha = int(self.alpha_input.text())
-        self.beta = int(self.beta_input.text())
-
-    def start_algorithm(self):
-        """Запуск муравьиного алгоритма."""
-        self.update_parameters()
-        self.ant_colony = AntColonyAlgorithm(
-            graph=self.graph,
-            evaporation_rate=self.evaporation_rate,
-            pheromone_intensity=self.pheromone_intensity,
-            alpha=self.alpha,
-            beta=self.beta
-        )
-        # Пример работы алгоритма (с реальной логикой добавим позже)
-        print("Алгоритм запущен.")
-        current_node = self.start_node
-        probabilities = self.ant_colony.calculate_probabilities(current_node, visited_nodes=[current_node])
-        print("Вероятности переходов:", probabilities)
-
-    def reset_graph(self):
-        """Сбрасывает граф и параметры к начальному состоянию."""
-        self.load_graph()
+    def update_end_node_combo(self):
+        self.end_node_combo.clear()
+        for node in sorted(self.graph.nodes):
+            self.end_node_combo.addItem(str(node))
 
     def update_canvas(self):
         """Очищает и перерисовывает граф на виджете pyqtgraph."""
         self.plot_widget.clear()
 
-        pos = nx.spring_layout(self.graph)  # Позиции для узлов
+        pos = self.positions  # Используем фиксированные позиции узлов
         edges = self.graph.edges(data=True)
 
         # Отрисовка рёбер
         for u, v, data in edges:
-            x = [pos[u][0], pos[v][0]]
-            y = [pos[u][1], pos[v][1]]
-            line = pg.PlotCurveItem(x, y, pen=pg.mkPen('gray', width=2))  # Серые линии для рёбер
+            pheromone = data['pheromone']
+            weight = data['weight']
+            line = pg.PlotCurveItem(
+                [pos[u][0], pos[v][0]], [pos[u][1], pos[v][1]],
+                pen=pg.mkPen(color=(200, 200, 200), width=2)
+            )
             self.plot_widget.addItem(line)
 
             # Добавляем текст для расстояния
             mid_x = (pos[u][0] + pos[v][0]) / 2
             mid_y = (pos[u][1] + pos[v][1]) / 2
-            distance_text = pg.TextItem(str(data['weight']), anchor=(0.5, 0.5), color='black')  # Черный текст
-            distance_text.setFont(pg.QtGui.QFont("Arial", 10, pg.QtGui.QFont.Bold))  # Устанавливаем шрифт
-            distance_text.setPos(mid_x, mid_y)
-            self.plot_widget.addItem(distance_text)
+            text_item = pg.TextItem(f"{weight}", anchor=(0.5, 0.5), color='black')
+            text_item.setFont(pg.QtGui.QFont("Arial", 10))
+            text_item.setPos(mid_x, mid_y)
+            self.plot_widget.addItem(text_item)
 
         # Отрисовка узлов
         for node in self.graph.nodes:
             x, y = pos[node]
-            color = '#6fcf97' if node == self.start_node else '#eb5757'  # Зеленый для начальной вершины, красный для остальных
+            if node == self.start_node:
+                color = '#6fcf97'  # Зеленый для начальной вершины
+            elif node == self.end_node:
+                color = '#f2994a'  # Оранжевый для конечной вершины
+            else:
+                color = '#eb5757'  # Красный для остальных узлов
+
             scatter = pg.ScatterPlotItem([x], [y], symbol='o', size=30, brush=color)
             self.plot_widget.addItem(scatter)
 
             # Текст внутри узлов
+            text = pg.TextItem(str(node), anchor=(0.5, 0.5), color='white')
+            text.setFont(pg.QtGui.QFont("Arial", 12, pg.QtGui.QFont.Bold))
+            text.setPos(x, y)
+            self.plot_widget.addItem(text)
+    def get_start_node(self):
+        current_text = self.start_node_combo.currentText()
+        return int(current_text) if current_text else None
+
+    def get_end_node(self):
+        current_text = self.end_node_combo.currentText()
+        return int(current_text) if current_text else None
+
+    def update_start_node(self):
+        self.start_node = self.get_start_node()
+        self.update_canvas()
+
+    def update_end_node(self):
+        self.end_node = self.get_end_node()
+        self.update_canvas()
+
+    def start_algorithm(self):
+        self.ant_colony = AntColonyAlgorithm(
+            graph=self.graph,
+            evaporation_rate=float(self.evaporation_rate_input.text()),
+            pheromone_intensity=float(self.pheromone_intensity_input.text()),
+            alpha=int(self.alpha_input.text()),
+            beta=int(self.beta_input.text())
+        )
+        self.num_ants = int(self.num_ants_input.text())
+        self.run_iterations()
+
+    def stop_algorithm(self):
+        if hasattr(self, 'timer') and self.timer.isActive():
+            self.timer.stop()
+
+    def run_iterations(self):
+        self.iteration = 0
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.run_single_iteration)
+        self.timer.start(1000)
+
+    def run_single_iteration(self):
+        """Итерация: моделирование движения муравьев и обновление феромонов."""
+        if self.iteration >= 20:  # Завершаем после 20 итераций
+            self.timer.stop()
+            return
+
+        all_paths = []
+        for _ in range(self.num_ants):
+            path, path_length = self.ant_colony.find_path(self.start_node, self.end_node)
+            print(f"Ant path: {path}, Length: {path_length}")  # Вывод пути муравья
+            if path_length < float('inf'):  # Исключаем тупиковые пути
+                all_paths.append((path, path_length))
+
+            self.visualize_ant_movement(path)
+
+        if all_paths:
+            best_path = min(all_paths, key=lambda x: x[1])[0]  # Берём путь с минимальной длиной
+            print(f"Best path: {best_path}")  # Вывод лучшего пути
+            self.ant_colony.update_pheromones(all_paths, best_path)
+
+        self.plot_widget.clear()  # Удаляем предыдущие рёбра
+        self.visualize_pheromones()  # Отображаем текущее состояние
+
+        self.iteration += 1
+
+    def visualize_ant_movement(self, path):
+        """Отображение движения муравья по графу."""
+        if not path:
+            return
+
+        pos = self.positions
+        for i in range(len(path) - 1):
+            u, v = path[i], path[i + 1]
+            x = [pos[u][0], pos[v][0]]
+            y = [pos[u][1], pos[v][1]]
+
+            # Рисуем путь муравья
+            line = pg.PlotCurveItem(x, y, pen=pg.mkPen('yellow', width=3))
+            self.plot_widget.addItem(line)
+            QApplication.processEvents()  # Обновление интерфейса
+            QTimer.singleShot(300, lambda: self.plot_widget.removeItem(line))  # Удаляем линию через 300 мс
+
+    # Удаляем линию через 300 мс
+    def visualize_initial_graph(self):
+        """Отображает граф с начальным состоянием."""
+        self.plot_widget.clear()
+        pos = self.positions  # Фиксированные позиции узлов
+
+        # Отрисовка рёбер с одинаковой толщиной
+        for u, v, data in self.graph.edges(data=True):
+            line = pg.PlotCurveItem(
+                [pos[u][0], pos[v][0]], [pos[u][1], pos[v][1]],
+                pen=pg.mkPen(color=(200, 200, 200), width=2)  # Начальная ширина
+            )
+            self.plot_widget.addItem(line)
+
+        # Отрисовка узлов
+        for node in self.graph.nodes:
+            x, y = pos[node]
+            color = '#6fcf97' if node == self.start_node else '#f2994a' if node == self.end_node else '#eb5757'
+            scatter = pg.ScatterPlotItem([x], [y], symbol='o', size=30, brush=color)
+            self.plot_widget.addItem(scatter)
+
+            text = pg.TextItem(str(node), anchor=(0.5, 0.5), color='white')
+            text.setFont(pg.QtGui.QFont("Arial", 12, pg.QtGui.QFont.Bold))
+            text.setPos(x, y)
+            self.plot_widget.addItem(text)
+
+    def visualize_best_path(self, best_path):
+        self.plot_widget.clear()
+        pos = self.positions
+        edges = self.graph.edges(data=True)
+
+        for u, v, data in edges:
+            pheromone = data['pheromone']
+            line = pg.PlotCurveItem(
+                [pos[u][0], pos[v][0]], [pos[u][1], pos[v][1]],
+                pen=pg.mkPen(color=(200, 200, 200, int(pheromone * 255)), width=2)
+            )
+            self.plot_widget.addItem(line)
+
+        for node in self.graph.nodes:
+            x, y = pos[node]
+            if node == self.start_node:
+                color = '#6fcf97'
+            elif node == self.end_node:
+                color = '#f2994a'
+            else:
+                color = '#eb5757'
+
+            scatter = pg.ScatterPlotItem([x], [y], symbol='o', size=30, brush=color)
+            self.plot_widget.addItem(scatter)
+
+            text = pg.TextItem(str(node), anchor=(0.5, 0.5), color='white')
+            text.setFont(pg.QtGui.QFont("Arial", 12, pg.QtGui.QFont.Bold))
+            text.setPos(x, y)
+            self.plot_widget.addItem(text)
+
+        if best_path:
+            for i in range(len(best_path) - 1):
+                u, v = best_path[i], best_path[i + 1]
+                x = [pos[u][0], pos[v][0]]
+                y = [pos[u][1], pos[v][1]]
+                path_line = pg.PlotCurveItem(x, y, pen=pg.mkPen('blue', width=4))
+                self.plot_widget.addItem(path_line)
+
+    def visualize_pheromones(self):
+        """Обновляет граф с отображением феромонов на рёбрах."""
+        self.plot_widget.clear()
+        pos = self.positions
+
+        # Отрисовка рёбер с толщиной, зависящей от уровня феромонов
+        for u, v, data in self.graph.edges(data=True):
+            pheromone = data['pheromone']
+            intensity = max(50, min(int(pheromone * 255), 255))  # Для прозрачности
+            width = max(1, int(pheromone * 10))  # Толщина зависит от феромонов
+            line = pg.PlotCurveItem(
+                [pos[u][0], pos[v][0]], [pos[u][1], pos[v][1]],
+                pen=pg.mkPen(color=(200, 0, 0, intensity), width=width)
+            )
+            self.plot_widget.addItem(line)
+
+        # Отображение узлов
+        for node in self.graph.nodes:
+            x, y = pos[node]
+            color = '#6fcf97' if node == self.start_node else '#f2994a' if node == self.end_node else '#eb5757'
+            scatter = pg.ScatterPlotItem([x], [y], symbol='o', size=30, brush=color)
+            self.plot_widget.addItem(scatter)
+
             text = pg.TextItem(str(node), anchor=(0.5, 0.5), color='white')
             text.setFont(pg.QtGui.QFont("Arial", 12, pg.QtGui.QFont.Bold))
             text.setPos(x, y)
